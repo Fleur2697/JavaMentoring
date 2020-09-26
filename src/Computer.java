b449cb8d2c446eaa5f979e75a8d087bd473200f9
Olha Koloskova <olha.koloskova@wickes.co.uk>
2020-09-26 23:22:25 +0300

    add first home task

---
+++ src/Computer.java
@@ -0,0 +1,12 @@
+public class Computer {
+    private String brand;
+    private float screenSize;
+    private int numberOfCores;
+    private String type;
+
+    public Computer(String brand, float screenSize) {
+        this.brand = brand;
+        this.screenSize = screenSize;
+    }
+
+}
---
+++ src/MainHomeWork.java
@@ -0,0 +1,30 @@
+public class MainHomeWork {
+    public static void main(String[] args) {
+        //Task 1
+        HomeWork_1.task1();
+
+        //Task 2
+        HomeWork_1.task2();
+
+        //Task 3
+        HomeWork_1.task3Sum();
+        HomeWork_1.task3Multuply();
+        HomeWork_1.task3Divide();
+        HomeWork_1.task3Substract();
+        HomeWork_1.task3Remainder();
+
+        //Task 4
+        int[] someArray = {5, 6, 7, 12, -5, 32, 43};
+        System.out.println(HomeWork_1.task4ArraySum(someArray));
+
+        //Task 5
+        int[] someArray1 = {5, 6, 7, 12, -5, 32, 43, 6, 12};
+        HomeWork_1.task5DuplicateVar1(someArray1);
+
+        //Task 6
+
+        HomeWork_1.task6ConcatLogic("PHP Exercises and ", "Python Exercises");
+
+
+    }
+}
